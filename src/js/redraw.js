cb31d5165bd40ef7dceacb935404f3707805dd6d
swanmo <swanmo@gmail.com>
2015-09-30 06:49:34 +0200

    Options for loading and unloading canvas

--- src/js/redraw.js
+++ src/js/redraw.js
@@ -4,6 +4,17 @@ import EventAggregator from './event-aggregator.js';
 import ControlsDispatcher from './controls/controls-dispatcher.js';
 
 var redrawNs = {tools: {}};
+
+   function dataURItoBlob(encodedData) {
+    var decodedData = atob(encodedData.split(',')[1]);
+    var array = []; // 8-bit unsigned array
+    for(var i = 0; i < decodedData.length; i++) {
+      array.push(decodedData.charCodeAt(i));
+    }
+    return new Blob([new Uint8Array(array)], {type: 'image/png'});
+  }
+
+
 /**
  * Main module.
  * @module redraw
@@ -18,31 +29,50 @@ class Redraw {
     constructor(imgElement, options) {
 		var events = new EventAggregator();
 
-		this.canvas = new Canvas(imgElement); // Needs defactor
+		this._canvas = new Canvas(imgElement); // Needs defactor
+
+		if (options.jsonContent) {
+			this._canvas.canvas.loadFromJSON(options.jsonContent);
+		}
+		
 
 		var controlsDispatcher = new ControlsDispatcher(events);
 		this.tools = [];
-		this.initializeTools(events, this.canvas.canvasContainer);
+		this.initializeTools(events, this._canvas.canvasContainer);
     }
+
+    toBase64URL() {
+    	return this._canvas.canvas.toDataURL('png');
+    }
+    toDataBlob() {
+		return dataURItoBlob(this._canvas.canvas.toDataURL('png'));
+    }
+	toJson(includeImage) {
+		var x = this._canvas.canvas.toObject();
+		if (!includeImage) {
+			delete x.backgroundImage;
+		}
+		return JSON.stringify(x);
+    }
+
+    
 
     initializeTools (events) {
     	var controls = new ControlsDispatcher(events);
-    	controls.setupTools(redrawNs.tools, this.canvas.canvasContainer);
+    	controls.setupTools(redrawNs.tools, this._canvas.canvasContainer);
 
 		for (var toolName in redrawNs.tools) {
-			new redrawNs.tools[toolName].toolFn(this.canvas, events);
+			new redrawNs.tools[toolName].toolFn(this._canvas, events);
 		}
     }
 }
 
-
+redrawNs.Annotation = Redraw;
 redrawNs.registerTool = function(_name, _toolFn, _options) {
 	redrawNs.tools[_name] = {address: _name, toolFn: _toolFn, options: _options};
-
 };
 
 
-
 var b = new Browser();
-b.appendToWindow('Redraw', Redraw);
+//b.appendToWindow('Redraw', Redraw);
 b.appendToWindow('redraw', redrawNs);
