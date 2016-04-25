7e9bf82054c7fb2b83e51e706c30ccbe26f3285b
swanmo <swanmo@gmail.com>
2016-04-25 23:00:34 +0200

    Tools configuration

--- src/js/tools/setup/arrow-setup.js
+++ src/js/tools/setup/arrow-setup.js
@@ -2,12 +2,10 @@ import CONST from '../../canvas-const.js';
 import Browser from '../../browser-api.js';
 import ArrowTool from '../arrow.js';
 
-/** Default options for tools initialization */
-var toolProps = {
+/** Default options for arrow tools initialization */
+var defaultToolProps = {
     label: 'Arrow',
     color:CONST.DEFAULT_COLOR,
     activeColor:'#55f'
 };
-if (!global && window && window.redraw) {
-    window.redraw.registerTool(CONST.TOOL.ARROW, ArrowTool, toolProps);
-}
+(new Browser()).getFromWindow('redraw').registerTool(CONST.TOOL.ARROW, ArrowTool, defaultToolProps);
